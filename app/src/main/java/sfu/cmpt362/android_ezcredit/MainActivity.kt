b013a396517ff03eb0299727bc8105449f1fe9cf
Ayush Arora <ayusharora10hg@gmail.com>
2025-11-23 13:19:31 -0800

    Added Reminder Sending Object

--- app/src/main/java/sfu/cmpt362/android_ezcredit/MainActivity.kt
+++ app/src/main/java/sfu/cmpt362/android_ezcredit/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import sfu.cmpt362.android_ezcredit.ui.NavigationDrawerScreen
 import sfu.cmpt362.android_ezcredit.ui.theme.Android_EZCreditTheme
+import sfu.cmpt362.android_ezcredit.utils.ReminderScheduler
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            ReminderScheduler.scheduleInvoiceReminders(this)
             Android_EZCreditTheme {
                 NavigationDrawerScreen()
             }
